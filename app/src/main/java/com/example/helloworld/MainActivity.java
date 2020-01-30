73e6a53e7fff558e99081d4aef4b7f09c05002f5
Cole Thomson <cthomson4@wisc.edu>
2020-01-30 16:54:34 -0600

    Added TextView on Windows Desktop

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView txtView; // Made this on Windows Laptop
 
+    TextView txtView2; // On Windows Desktop
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
